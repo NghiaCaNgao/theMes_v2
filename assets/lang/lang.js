eab7c5a8e70396a57f606db757c6da8119d28ff1
nghia <nghiacangao>
2020-06-06 21:16:17 +0700

    add lang

---
+++ assets/lang/lang.js
@@ -0,0 +1,102 @@
+var lang = [
+    "lab_img_link" = {
+        "vi": "Link ảnh",
+        "en": "image link"
+    },
+    "lab_style" = {
+        "vi": "Style",
+        "en": "Style"
+    },
+    "lab_code_css" = {
+        "vi": "Css code",
+        "en": "Css code"
+    },
+    "lab_settings_json" = {
+        "vi": "Mã cài đặt",
+        "en": "Settings json"
+    },
+    "btn_get_style:" = {
+        "vi": "Lấy style",
+        "en": "Get style"
+    },
+    "btn_get_settings" = {
+        "vi": "Lấy mã cài đặt",
+        "en": "Get settings"
+    },
+    "btn_restore_settings" = {
+        "vi": "Khôi phục cài đặt gốc",
+        "en": "Restore settings"
+    },
+    "btn_add_new" = {
+        "vi": "Thêm mới",
+        "en": "Add new"
+    },
+    "btn_more_settings" = {
+        "vi": "Cài đặt nâng cao",
+        "en": "More settings"
+    },
+    "btn_minify_css" = {
+        "vi": "Minify css",
+        "en": "Minify css"
+    },
+    "btn_unminify_css" = {
+        "vi": "Unminìy css",
+        "en": "Unminìy css"
+    },
+    "btn_learn_css" = {
+        "vi": "Học css",
+        "en": "Learn css"
+    },
+    "btn_classes" = {
+        "vi": "Một số lớp sử dụng trong messenger",
+        "en": "Some classes in messgenger"
+    },
+    "btn_save" = {
+        "vi": "Save",
+        "en": "Save"
+    },
+    "placeholder_uid" = {
+        "vi": "uid",
+        "en": "uid"
+    },
+    "placeholder_ủl" = {
+        "vi": "url",
+        "en": "url"
+    },
+    "placeholder_code_settings" = {
+        "vi": "Bỏ mã cài đặt vào ô này",
+        "en": "Insert your settings code here"
+    },
+    "placeholder_code_css" = {
+        "vi": "Chèn mã css ở đây. Nếu bạn không biết về css, bạn có thể tham khảo link bên dưới",
+        "en": "Insert css code here. If you don't know css, you can quickly learn about with some links below."
+    },
+    "des_code" = {
+        "vi": "Bạn có thể chèn một số mã css để tự tùy biến giao diện trang messenger cho chính bạn",
+        "en": "On this page, you can edit your messenger's appearance by css code"
+    },
+    "des_image_with_id" = {
+        "vi": "Bạn có thể lựa chọn hình ảnh hiển thi thị tùy theo id của bạn bè",
+        "en": "On this page, you can choose what image will apear when you click on friend chat tab  "
+    },
+    "des_export" = {
+        "vi": "Bạn có thể xuất hoặc nhập settings từ một đoạn mã có sẵn",
+        "en": "On this page, you can export or import settings from a available code"
+    },
+    "inform_invalid_image" = {
+        "vi": "Link của hình ảnh không hợp lệ",
+        "en": "Invalid image link"
+    },
+    "inform_too_long_url" = {
+        "vi": "Url hình ảnh quá dài để ghi",
+        "en": "Too long url"
+    },
+    "menu_default_image" = {
+        "vi": "Đặt làm nền mặc định",
+        "en": "Set as default images"
+    },
+    "menu_id_image" = {
+        "vi": "Đặt cho một ai đó",
+        "en": "Set for someone"
+    }
+];
---
+++ assets/lang/setLang.js
@@ -0,0 +1,48 @@
+function imagePage() {
+
+}
+
+function stylePage() {
+
+}
+
+function imageIdPage() {
+
+}
+
+function exportPage() {
+
+}
+
+function defaultPage() {
+
+}
+
+function setPage(page, lang) {
+    switch (page) {
+        case "index":
+            {
+                break;
+            }
+        case "style":
+            {
+                break;
+            }
+        case "imageId":
+            {
+                break;
+            }
+        case "export":
+            {
+                break;
+            }
+        default:
+            {
+
+            }
+    }
+}
+
+function setInformJs() {
+
+}
